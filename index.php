05625f0416940a3b5d29279656ddaa4b7d1e00f0
63070129 <73679698+63070129@users.noreply.github.com>
2020-10-30 10:53:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
-echo "<h2>ภคิน จันทร์ทอง เอิร์ธ</h2>";
-echo <img src="cat.jpg"><br>;
-echo <img src="cat1.jpg"><br>;
-?>
+<html>
+    <head>
+        <title>นาย ภคิน จันทร์ทอง เอิร์ธ</title>
+    </head>
+    <body>
+        <img src="cat.jpg">
+        <img src="cat1.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+        
+        <h2>เรื่องตลก3บรรทัด</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
